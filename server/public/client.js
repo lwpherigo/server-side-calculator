8fac60745e5ef879fd24ea29b3de8b4293ac6692
Lucas Pherigo <lucas.pherigo@gmail.com>
2019-11-24 12:02:59 -0600

    added operation buttons

--- server/public/client.js
+++ server/public/client.js
@@ -4,13 +4,28 @@ $(document).ready(init);
 
 function init() {
     console.log('DOM is totally ready.')
+    $('.js-btn-divide').on('click', divideButton);
+    $('.js-btn-multiply').on('click', multiplyButton);
+    $('.js-btn-subtract').on('click', subtractButton);
     $('.js-btn-add').on('click', addButton);
     $('.js-btn-equals').on('click', grabEquation);
     $('.js-btn-clear').on('click', clearInputs);
 }
 
-function addButton() {
-    
+function addButton(){
+    sign('add');
+}
+function subtractButton(){
+    sign('subtract');
+}
+function multiplyButton(){
+    sign('multiply');
+}
+function divideButton(){
+    sign('divide');
+}
+function sign(x) {
+    operator = x;
 }
 
 function grabEquation() {
@@ -18,11 +33,11 @@ function grabEquation() {
     let valueOne = $inputOne.val();
     const $inputThree = $('.js-valueThree');
     let valueThree = $inputThree.val();
-    const $add = $('.js-btn-add');
+    let add = $('.js-btn-add');
 
     const mathProblem = {
         val1: valueOne,
-        val2: ,
+        val2: valueTwo,
         val3: valueThree,
     }
 }
